d323bf2dd82ae0380140f96864b2580cc2ea95cb
Dmytro Pustovit <dipustovit@gmail.com>
2019-07-11 21:28:39 +0300

    Build(config): parse and merge configs with env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,30 @@
-// import config from '../etc/config.json';
+import path from 'path';
+import fs from 'fs-extra';
+
+function loadJSON(filePath, defaults = {}) {
+    const file = path.resolve(__dirname, filePath);
+
+    return fs.exists(file)
+        ? JSON.parse(fs.readFileSync(file, 'utf8')) // eslint-disable-line no-sync
+        : defaults;
+}
+
+const appConfig = {
+    ...loadJSON('../etc/config.json.defaults'),
+    ...loadJSON('../etc/config.json')
+};
+const env = {};
+
+Object.entries(process.env)
+    .forEach(([ key, value ]) => {
+        env[key.toLowerCase()] = value;
+    });
 
 // TODO validate
-export default {};
+// TODO async loading
+
+export default {
+    ...appConfig,
+    ...env
+};
 
