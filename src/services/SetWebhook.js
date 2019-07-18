5e92b6b416312b13f7006ba5257dd66aaa06491b
Dmytro Pustovit <dipustovit@gmail.com>
2019-07-19 01:33:12 +0300

    Chore: (refactor) remove waste logs

--- src/services/SetWebhook.js
+++ src/services/SetWebhook.js
@@ -6,22 +6,13 @@ const webhookUrl = `${config.host}${config.prefix}/updates/${config.webhook}`;
 
 export default class SetWebhook extends Base {
     async run({ url = webhookUrl }, options) {
-        try {
-            console.log(1);
-            let current = await telegram.getWebhook();
+        let current = await telegram.getWebhook();
 
-            console.log(2);
-
-            if (options.verbose) console.log('current WebhookUrl: ', current);
-            if (options.confirm) {
-                current = await telegram.setWebhook(url);
-            }
-
-            return { data: current  };
-        } catch (e) {
-            console.log('before');
-            console.log('after');
-            throw e;
+        if (options.verbose) console.log('current WebhookUrl: ', current);
+        if (options.confirm) {
+            current = await telegram.setWebhook(url);
         }
+
+        return { data: current  };
     }
 }
